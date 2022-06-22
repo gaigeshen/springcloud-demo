70369c47ced7a16b3d066261862ecda09905c3a6
gaigeshen <gaigeshen@qq.com>
2022-06-22 11:15:54 +0800

    change Results.java

--- springcloud-demo-eureka/springcloud-demo-eureka-api/src/main/java/work/gaigeshen/springcloud/demo/eureka/api/commons/web/Results.java
+++ springcloud-demo-eureka/springcloud-demo-eureka-api/src/main/java/work/gaigeshen/springcloud/demo/eureka/api/commons/web/Results.java
@@ -1,70 +1,51 @@
 package work.gaigeshen.springcloud.demo.eureka.api.commons.web;
 
-import java.util.Objects;
-
 /**
- * 响应结果工具类
  *
  * @author gaigeshen
  */
 public abstract class Results {
 
-  /**
-   * 无需调用此方法
-   */
   private Results() {  }
 
-  /**
-   * 校验响应结果是否是成功的
-   *
-   * @param result 响应结果
-   * @return 是否是成功的
-   * @param <D> 响应结果中包含的数据的类型
-   */
-  public static <D> boolean isSuccess(Result<D> result) {
-    return Objects.equals(Result.DEFAULT_CODE, result.getCode());
+  public static <D> D getData(Result<D> result) {
+    return getData(result, new ResultValidator<>() { }, new ResultExceptionProducer<>() { });
   }
 
-  /**
-   * 检查响应结果然后获取其中的数据
-   *
-   * @param result 响应结果
-   * @param provider 如果响应结果不是成功的则会调用此异常提供器获取异常然后将其抛出
-   * @return 响应结果中的数据
-   * @param <D> 响应结果中包含的数据的类型
-   */
-  public static <D> D checkAndGetData(Result<D> result, ExceptionProviderr<D> provider) {
-    if (isSuccess(result)) {
-      return result.getData();
+  public static <D> D getData(Result<D> result, ResultValidator<D> validator) {
+    return getData(result, validator, new ResultExceptionProducer<>() { });
+  }
+
+  public static <D> D getData(Result<D> result, ResultExceptionProducer<D> producer) {
+    return getData(result, new ResultValidator<>() { }, producer);
+  }
+
+  public static <D> D getData(Result<D> result, ResultValidator<D> validator, ResultExceptionProducer<D> producer) {
+    if (validator.validate(result)) {
+      throw producer.produce(result);
     }
-    throw provider.provide(result);
+    return result.getData();
   }
 
   /**
-   * 检查响应结果然后获取其中的数据
-   *
-   * @param result 响应结果
-   * @return 响应结果中的数据
-   * @param <D> 响应结果中包含的数据的类型
-   */
-  public static <D> D checkAndGetData(Result<D> result) {
-    return checkAndGetData(result, r -> new ResultException(r, r.getMessage()));
-  }
-
-  /**
-   * 异常提供器
    *
    * @author gaigeshen
    */
-  @FunctionalInterface
-  public interface ExceptionProviderr<D> {
+  public interface ResultValidator<D> {
 
-    /**
-     * 调用此方法将产生运行时异常对象
-     *
-     * @param result 响应结果
-     * @return 运行时异常对象
-     */
-    RuntimeException provide(Result<D> result);
+    default boolean validate(Result<D> result) {
+      return Result.DEFAULT_CODE == result.getCode();
+    }
+  }
+
+  /**
+   *
+   * @author gaigeshen
+   */
+  public interface ResultExceptionProducer<D> {
+
+    default ResultException produce(Result<D> result) {
+      return new ResultException(result, result.getMessage());
+    }
   }
 }
